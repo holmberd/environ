2932709c75ee9c3c62a17c70977c66783b0d5c37
Dag Holmberg <holmberd@users.noreply.github.com>
2017-08-22 06:29:00 -0700

    Create workstation-setup.sh

---
+++ workstation-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Basic Ubuntu Programming Workstation Setup
+
+set -e
+
+sudo apt-get update
+sudo apt-get build-essential
+sudo apt-get htop
+sudo apt-get tree
+sudo apt-get git
+# sudo apt-get sublimetext3
+# sudo apt-get oh-my-zsh
+
