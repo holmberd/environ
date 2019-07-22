027c3a8a204c904ad510a4db6ad89d77420cd9b7
Dag Holmberg <holmberd@users.noreply.github.com>
2019-07-22 06:45:34 -0700

    Update workstation-setup.sh

--- workstation-setup.sh
+++ workstation-setup.sh
@@ -3,11 +3,32 @@
 
 set -e
 
-sudo apt-get update
-sudo apt-get build-essential
-sudo apt-get htop
-sudo apt-get tree
-sudo apt-get git
+sudo apt update
+sudo apt install
+sudo apt install build-essential net-tools
+sudo apt install curl
+sudo apt install tree
+
+# Dev
+sudo apt install git
+sudo apt install npm
+# NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+nvm install node
+# Python
+sudo apt install python3-pip
+pip install virtualenv
+# AWS
+pip3 install awscli --upgrade --user
+
+
+# System
+sudo apt install htop
+sudo apt install inxi
+
+# Network
+sudo apt-get install openvpn network-manager-openvpn network-manager-openvpn-gnome
+
 # sudo apt-get sublimetext3
 # sudo apt-get oh-my-zsh
 
